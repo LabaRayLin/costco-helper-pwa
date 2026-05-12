17c93634839d6f6dd30e31edcf02b6169cedd393
LabaRayLin <labaray123@gmail.com>
2026-05-12 13:46:17 +0800

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -40,7 +40,7 @@ async function scrape() {
 
     try {
         // 第一波抓取：所有商品的總覽
-        const initialUrl = `https://www.costco.com.tw/rest/v2/taiwan/products/search?query=:relevance&fields=products(code,name,summary,price(FULL),images(DEFAULT),stock(FULL),averageRating,variantOptions),pagination(totalPages,totalResults,number)&pageSize=100&lang=zh_TW&curr=TWD&currentPage=0`;
+        const initialUrl = `https://www.costco.com.tw/rest/v2/taiwan/products/search?query=:relevance:allCategories&fields=FULL&pageSize=100&lang=zh_TW&curr=TWD&currentPage=0`;
         console.log(`正在發送初始請求: ${initialUrl}`);
         const firstPage = await fetchJSON(initialUrl);
         
@@ -62,7 +62,7 @@ async function scrape() {
 
         for (currentPage = 0; currentPage < pagesToFetch; currentPage++) {
             console.log(`[${currentPage + 1}/${pagesToFetch}] 正在抓取商品...`);
-            const url = `https://www.costco.com.tw/rest/v2/taiwan/products/search?query=:relevance&fields=products(code,name,summary,price(FULL),images(DEFAULT),stock(FULL),averageRating,variantOptions),pagination(totalPages,totalResults,number)&pageSize=100&lang=zh_TW&curr=TWD&currentPage=${currentPage}`;
+            const url = `https://www.costco.com.tw/rest/v2/taiwan/products/search?query=:relevance:allCategories&fields=FULL&pageSize=100&lang=zh_TW&curr=TWD&currentPage=${currentPage}`;
             
             try {
                 const data = await fetchJSON(url);
